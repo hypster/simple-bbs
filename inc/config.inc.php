b4d96d4aa9a77d816160015a422941480a692d1c
Yiping Huang <yiping.huang@nov.com>
2022-10-02 16:16:14 +0200

    添加父板块删除模块

---
+++ inc/config.inc.php
@@ -0,0 +1,10 @@
+<?php 
+header('Content-type:text/html;charset=utf-8');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_DATABASE','bbs');
+define('DB_PORT',3306);
+
+
+?>
